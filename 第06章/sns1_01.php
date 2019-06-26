e87dafaa5fa6cab5d87936911a073b03cff17a63
tsunako275 <50385597+tsunako275@users.noreply.github.com>
2019-06-26 11:01:12 +0900

    Add files via upload

---
+++ 第06章/sns1_01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SNS1</title>
+</head>
+<body>
+<form action="sns2.php" method="post">
+名前
+<div><input type="text" name="n"></div>
+メッセージ
+<div><textarea name="m"></textarea></div>
+<input type="submit" value="送信するよ!">
+</form>
+</body>
+</html>
---
+++ 第06章/sns1_03.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SNS1</title>
+</head>
+<body>
+<form action="sns2.php" method="post">
+名前
+<div><input type="text" name="n"></div>
+メッセージ
+<div><textarea name="m"></textarea></div>
+<input type="submit" value="送信するよ!">
+</form>
+<?php
+$db = new PDO("mysql:host=localhost;dbname=db","root","root");
+$ps = $db->query("SELECT * FROM tb");
+while ($r = $ps->fetch()){
+    print "{$r['ban']} {$r['nam']} {$r['dat']} {$r['mes']} <hr>";
+}
+?>
+</body>
+</html>
